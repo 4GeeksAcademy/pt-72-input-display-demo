66ded7db6a9cda5d79c74b5f313523ab3e57b96e
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2024-10-04 22:47:19 +0000

    refactored and cleaned up

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,21 +3,15 @@ import React, { useEffect, useState } from "react";
 
 const Home = () => {
 	const [users, setUsers] = useState([]);
-	const [nameInput, setNameInput] = useState("");
-	const [emailInput, setEmailInput] = useState("");
-	const [passwordInput, setPasswordInput] = useState("");
-	const [input, setInput] = useState({name: "", email: "", password: ""})
+	const [input, setInput] = useState({ name: "", email: "", password: "" })
 	const [showName, setShowName] = useState(true)
 	const [showEmail, setShowEmail] = useState(true)
 	const [showPassword, setShowPassword] = useState(true)
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		let user = { name: nameInput, email: emailInput, password: passwordInput }
-		setUsers([...users, user])
-		setNameInput("");
-		setEmailInput("");
-		setPasswordInput("");
+		setUsers([...users, input])
+		setInput({ name: "", email: "", password: "" })
 	}
 
 	const hideUserInfo = (e, type) => {
@@ -65,23 +59,22 @@ const Home = () => {
 		<div className="w-25 mt-5 mx-auto">
 			<div>
 				<h1>Name</h1>
-				<input type="text" value={nameInput} onChange={(e) => setInput({name: e.target.value, email:input.email, password: input.passord})} />
+				<input type="text" value={input.name} onChange={(e) => setInput({ name: e.target.value, email: input.email, password: input.password })} />
 			</div>
 			<div>
 				<h1>Email</h1>
-				<input type="email" value={emailInput} onChange={(e) => setEmailInput(e.target.value)} />
+				<input type="email" value={input.email} onChange={(e) => setInput({ name: input.name, email: e.target.value, password: input.password })} />
 			</div>
 			<div>
 				<h1>Password</h1>
-				<input type="password" value={passwordInput} onChange={(e) => setPasswordInput(e.target.value)} onKeyDown={(e) => e.key === 'Enter' ? handleSubmit(e, "password") : console.log("not enter")} />
+				<input type="password" value={input.password} onChange={(e) => setInput({ name: input.name, email: input.email, password: e.target.value })} onKeyDown={(e) => e.key === 'Enter' ? handleSubmit(e) : ""} />
 			</div>
 			<button className="btn btn-info my-2" onClick={(e) => handleSubmit(e)}>Add User</button>
 			{users?.map((thisUser, index) => (
-				<div>
-					{console.log(thisUser.name)}
+				<div key={index}>
 					<div className={showName == true ? "" : "d-none"}><span>{thisUser.name}</span> <input type="checkbox" onChange={(e) => hideUserInfo(e, "name")} title="check to hide information" /></div>
-					<div><span>{thisUser.email}</span> <input type="checkbox" onChange={(e) => hideUserInfo(e, "email")} title="check to hide information" /></div>
-					<div><span>{thisUser.password}</span> <input type="checkbox" onChange={(e) => hideUserInfo(e, "password")} title="check to hide information" /></div>
+					<div className={showEmail == true ? "" : "d-none"}><span>{thisUser.email}</span> <input type="checkbox" onChange={(e) => hideUserInfo(e, "email")} title="check to hide information" /></div>
+					<div className={showPassword == true ? "" : "d-none"}><span>{thisUser.password}</span> <input type="checkbox" onChange={(e) => hideUserInfo(e, "password")} title="check to hide information" /></div>
 					<hr></hr>
 				</div>
 			))
